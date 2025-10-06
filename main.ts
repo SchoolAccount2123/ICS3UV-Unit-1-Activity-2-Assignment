658cdcf92d30ebf884d9b9eeb5734b2d97982fa2
Connor-Wilkins <connor.wilkins@stu.ocsb.ca>
2025-10-06 19:34:09 +0000

    Started assignment code

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * This is code for the Assignment: Output using Calculations
+ */
+
+
+
+// Code for converting temp of 34 degrees Celsius to Fahrenheit
+
+console.log("Converting the temprature of 34 degrees Celsius to Fehrenheit using the formula: F = 9/5 * C + 32")
+
+
+
+console.log("The answer is:", 9 / 5 * 34 + 32)
