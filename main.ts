77f6fed65df36b243ab15bf15aa232cf73f6ab14
Connor-Wilkins <connor.wilkins@stu.ocsb.ca>
2025-10-07 18:05:49 +0000

    completed the assignment code

--- main.ts
+++ main.ts
@@ -11,3 +11,21 @@ console.log("Converting the temprature of 34 degrees Celsius to Fehrenheit using
 
 
 console.log("The answer is:", 9 / 5 * 34 + 32)
+
+// Code for outputting the area of a circle with a radius of 14cm
+
+console.log("Outputting the area of a circle with the radius of 14cm using the formula A = Pi * r * r * r")
+
+console.log("The area is:", 3.14 * 14 * 14,"cm²")
+
+// Code for outputting the volume of a sphere with a radius of 4cm
+
+console.log("Outputting the volume of a sphere with a radius of 4cm using the formula V 4/3 * Pi * r * r * r")
+
+console.log("The volume is:", 4 / 3 * 3.14 * 4 * 4 * 4,"cm³")
+
+// Code for calculating the gross pay for an employee named fred who worked 40 hours witha  wage of $8.20
+
+console.log("This part of the code will calculate Fred's gross pay with his wage of $8.20 and hour after he worked 40 hours")
+
+console.log("His gross pay is: $",40 * 8.20)
